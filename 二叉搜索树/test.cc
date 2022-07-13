0ee851f6110f11d10d1d83c00d8eb2de93fdb9c7
gueFDF <3237455241@qq.com>
2022-07-13 19:18:06 +0800

    修改了bug

--- 二叉搜索树/test.cc
+++ 二叉搜索树/test.cc
@@ -8,12 +8,30 @@ int main()
     tree.insert(tree.get_root(),9);
     tree.insert(tree.get_root(),6);
     tree.insert(tree.get_root(),3);
-    cout<<tree.get_root()->get_data()<<endl;
-    cout<<tree.Find(tree.get_root(),15)->get_data()<<endl;
-    cout<<tree.Find(tree.get_root(),9)->get_data()<<endl;
-    cout<<tree.Find(tree.get_root(),7)->get_data()<<endl;
-    cout<<tree.FindMax(tree.get_root())->get_data()<<endl;
-     tree.set_root(tree.Delete(tree.get_root(),15));
-    cout<<tree.FindMax(tree.get_root())->get_data()<<endl;
+    tree.insert(tree.get_root(),7);
+    tree.insert(tree.get_root(),9);
+    tree.insert(tree.get_root(),56);
+    tree.insert(tree.get_root(),3);
+    tree.insert(tree.get_root(),3333);
+    tree.insert(tree.get_root(),9);
+    tree.insert(tree.get_root(),6);
+    tree.insert(tree.get_root(),35);
+    tree.insert(tree.get_root(),73);
+    tree.insert(tree.get_root(),92);
+    tree.insert(tree.get_root(),64);
+    tree.insert(tree.get_root(),36);
+     tree.in_order(tree.get_root());
+    cout<<endl;
+     tree.Delete(tree.get_root(),73);
+     tree.in_order(tree.get_root());
+      cout<<endl;
+     tree.Delete(tree.get_root(),56);
+     tree.in_order(tree.get_root());
+     cout<<endl;
+tree.Delete(tree.get_root(),64);
+tree.in_order(tree.get_root());
+ cout<<endl;
+
+
             
 }
